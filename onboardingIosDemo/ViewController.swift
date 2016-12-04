1d969108d8b4fb53f6e93473260ef6814e5dcfa9
Mihir <mihir@mihirs-macbook-pro.local>
2016-12-04 18:03:28 -0500

    Initial Commit

---
+++ onboardingIosDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onboardingIosDemo
+//
+//  Created by Mihir on 2016-12-04.
+//  Copyright © 2016 Mihir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
